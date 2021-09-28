1bf463a73859639592e29ec98f1b560dab8bed5d
Explosion-Scratch <Explosion-Scratch@users.noreply.github.com>
2021-09-28 19:59:05 +0000

    deploy: 095198936640a9e9868afcd909b655174ad8759e

--- _nuxt/static/1632856173/hello/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1632856173",layout:"default",error:a,serverRendered:b,routePath:"\u002Fhello",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6a365619"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
