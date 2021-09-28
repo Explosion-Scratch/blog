c5b60efe21f79b4a43571f9fcf831666217edcd0
Explosion-Scratch <explosion.scratch@gmail.com>
2021-09-28 14:52:53 +0000

    Code editor component!!!

---
+++ components/CodeEditor.vue
@@ -0,0 +1,24 @@
+<template>
+  <iframe @load="init" class="codeeditor w-full h-full" src="https://yj639.csb.app/"></iframe>
+</template>
+
+<script>
+export default {
+    name: 'CodeEditor',
+    data(){
+        return {
+            id: null,
+        }
+    },
+    methods: {
+        init(){
+            this.$el.contentWindow.postMessage({fromParent: true, action: "language", data: this.language}, "*");
+            this.$el.contentWindow.postMessage({fromParent: true, action: "update", data: this.code}, "*")
+        }
+    },
+    props: {
+        language: {type: String, default: "html"},
+        code: {type: String, default: ""}
+    }
+}
+</script>
---
+++ pages/editor.vue
@@ -0,0 +1,8 @@
+<template>
+    <CodeEditor class="block h-96" language="html" code="<h1>Hello world!!!</h1>"/>
+</template>
+<script>
+export default {
+    layout: "home",
+}
+</script>
