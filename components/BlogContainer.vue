6f15ee07800aaee3569f65e4ffadf45dbdc4d4bc
Explosion-Scratch <explosion.scratch@gmail.com>
2021-09-28 00:57:50 +0000

    buttons and more

--- components/BlogContainer.vue
+++ components/BlogContainer.vue
@@ -2,7 +2,7 @@
   <section class="text-gray-600">
     <div class="p-10 w-full">
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 gap-4">
-        <BlogPost :info="post" :key="post.id || post.slug" v-for="post in items" />
+        <BlogPost :info="post" :key="post.id || post.slug" v-for="post in sortedItems" />
       </div>
     </div>
   </section>
@@ -19,6 +19,17 @@ export default {
 			default: () => [],
 		},
 	},
+	computed: {
+		sortedItems(){
+			var sorted = this.items.sort((a,b)=>{
+				var s = new Date(a.createdAt);
+				var e = new Date(b.createdAt);
+				return s.getTime()-e.getTime();
+			}).reverse();
+			console.log({sorted})
+			return sorted;
+		}
+	}
 };
 </script>
 
--- components/RippleButton.vue
+++ components/RippleButton.vue
@@ -10,10 +10,10 @@ export default {
     data(){
         return {id: null}
     },
-    mounted(){
+    async mounted(){
         this.id = ~~(Math.random() * 10000);
         if (process.browser){
-            this.$nextTick(() => (function ripple(el, opts = {}) {
+            (function ripple(el, opts = {}) {
                 const time = opts.time || (+el.getAttribute("data-time") || 1000) * 3;
                 const color = opts.color || el.getAttribute("data-color") || "currentColor";
                 const opacity = opts.opacity || el.getAttribute("data-opacity") || ".3";
@@ -50,7 +50,25 @@ export default {
                     }, time);
                     }, 1);
                 });
-            })(document.getElementById(`button_${this.id}`)));
+            })(await elementReady(`#button_${this.id}`))
+            function elementReady(selector) {
+                return new Promise((resolve, reject) => {
+                    const el = document.querySelector(selector);
+                    if (el) {resolve(el);}
+                    new MutationObserver((mutationRecords, observer) => {
+                    // Query for elements matching the specified selector
+                    Array.from(document.querySelectorAll(selector)).forEach((element) => {
+                        resolve(element);
+                        //Once we have resolved we don't need the observer anymore.
+                        observer.disconnect();
+                    });
+                    })
+                    .observe(document.documentElement, {
+                        childList: true,
+                        subtree: true
+                    });
+                });
+            }
         }
     }
 }
