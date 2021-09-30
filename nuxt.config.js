ff336c63d3948a22242aa6dcf7ff9205be4c83bf
Explosion-Scratch <explosion.scratch@gmail.com>
2021-09-30 15:32:23 +0000

    Minify html

--- nuxt.config.js
+++ nuxt.config.js
@@ -99,5 +99,21 @@ export default {
 
 	// Build Configuration: https://go.nuxtjs.dev/config-build
 	build: {
+		html:{
+		  minify:{
+			collapseBooleanAttributes: true,
+			decodeEntities: true,
+			minifyCSS: true,
+			minifyJS: true,
+			processConditionalComments: true,
+			removeEmptyAttributes: true,
+			removeRedundantAttributes: true,
+			trimCustomFragments: true,
+			useShortDoctype: true,
+			minifyURLs: true,
+			removeComments: true,
+			removeEmptyElements: true
+		  }
+		}
 	}
 };
