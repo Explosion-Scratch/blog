c825646e94fca3ee0435b7cd8e6dab5bcc6a6f56
Explosion-Scratch <explosion.scratch@gmail.com>
2021-10-01 14:06:25 +0000

    Callout

--- components/Callout.vue
+++ components/Callout.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="callout"><slot></slot></div>
+  <div class="callout">
+    <span class="emoji">{{emoji}}</span>
+    <span class="text inline-flex justify-center items-center"><slot></slot></span>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Callout"
+    name: "Callout",
+    props: {
+      emoji: {type: String, default: "💡"}
+    }
 }
 </script>
 
@@ -12,8 +18,18 @@ export default {
 @import "../static/css/base";
 
 .callout {
-    background: rgba($base-color, .1);
-    padding: 10px;
-    border-radius: 3px;
+  background: rgba($base-color, .2);
+  display: flex;
+  padding: 10px;
+  width: 100%;
+  color: lighten($base-color, 30);
+  border-radius: 5px;
+  .emoji {
+      font-size: 1.4rem;
+      padding-right: 10px;
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+  }
 }
 </style>
