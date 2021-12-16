01e527ec72efa24b6f0e1fb9edc3ac941572ec34
Explosion-Scratch <Explosion-Scratch@users.noreply.github.com>
2021-12-16 16:59:38 +0000

    deploy: 444b3081345aeb875a49a74024b81d757c62c030

---
+++ _nuxt/static/1639673976/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fexplaining-to-developers","\u002F","\u002Fhello","\u002Fnew-projects","\u002Fripple","\u002Fuseful-developer-resources","\u002Fcontact","\u002Fsearch","\u002Fcodeeditors"]})
